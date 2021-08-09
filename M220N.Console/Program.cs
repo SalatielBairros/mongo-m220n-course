7f1e9ffdf6e6a58cd64b002fd3084a00622399d4
Salatiel Bairros <salatiel.costabairros@gmail.com>
2021-08-08 21:39:39 -0300

    Commit inicial da solução

---
+++ M220N.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M220N.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
